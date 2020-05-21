b00008ec17979f4e41b0b42395cd771b0c0aa5d8
Patrick Dal Gobbo <juandalgobbo@Juans-MacBook-Air.local>
2020-05-21 18:22:26 -0400

    starting small with test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,5 @@
 const supertest = require("supertest");
-  expect(2 + 2).toBe(4);	const server = require("../server");
+const server = require("../server");
 
 test("GET /", async () => {
   const endpoint = "/";
@@ -8,5 +8,5 @@ test("GET /", async () => {
   const res = await supertest(server).get(endpoint);
   expect(res.statusCode).toBe(status);
   expect(res.type).toBe("application/json");
-  expect(res.body.message).toBe("Welcome to our API!");
+  expect(res.body.message).toBe("API up");
 });
